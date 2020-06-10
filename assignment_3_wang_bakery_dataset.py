b54bfe7c0a2a69357aa7240a5ca8586ebb0fb4b7
ruotingw <wruoting@gmail.com>
2020-06-09 21:54:54 -0400

    finished bakery assignment

--- assignment_3_wang_bakery_dataset.py
+++ assignment_3_wang_bakery_dataset.py
@@ -36,6 +36,31 @@ def main():
     maximum_baristas.reset_index(inplace=True)
     maximum_baristas.columns = ['Weekday', 'Maximum Baristas']
 
+    # Question 5
+    food_drink_items = df[['Item_Price', 'Classification']].groupby(['Classification']).agg(['mean'])
+    mean_drink_value = np.round(food_drink_items.loc['Drink'].values[0], 2)
+    mean_food_value = np.round(food_drink_items.loc['Food'].values[0], 2)
+
+    # Question 6
+    food_drink_items_sum = df[['Item_Price', 'Classification']].groupby(['Classification']).agg(['sum'])
+    total_drink_value = np.round(food_drink_items_sum.loc['Drink'].values[0], 2)
+    total_food_value = np.round(food_drink_items_sum.loc['Food'].values[0], 2)
+
+    # Question 7
+    items_group_by_date = df[['Year', 'Month', 'Day', 'Weekday', 'Transaction', 'Item']].groupby(['Year', 'Month', 'Day', 'Weekday', 'Item']).agg('count')
+    # Drop the dates that we used to group, reset the index so we can group by it again
+    items_group_by_date.index = items_group_by_date.index.droplevel([0, 1, 2])
+    items_group_by_date.reset_index(inplace=True)
+    list_of_days = items_group_by_date['Weekday'].unique()
+
+    # Question 9
+    classification_group_by_transactions = df[['Classification', 'Transaction', 'Item']].groupby(['Classification', 'Transaction']).agg('count')
+    classification_group_by_transactions.reset_index(inplace=True)
+    classification_group_by_transactions = classification_group_by_transactions[classification_group_by_transactions['Classification'] == 'Drink'].copy()
+    # filter out unnecessary columns and groupy by transaction
+    drinks_by_transactions = classification_group_by_transactions[['Transaction', 'Item']].groupby(['Transaction']).agg('mean')
+
+
     print('Question 1')
     print('(a) What is the busiest hour in terms of most transactions per hour?')
     print('Here is the sorted number of transactions to hours list:')
@@ -84,8 +109,43 @@ def main():
     print(maximum_baristas.T)
 
     print('\nQuestion 5')
-    
+    print('Classifications were done using Google to my best knowledge.')
+    print('Average price of drink item: ${}'.format(mean_drink_value))
+    print('Average price of a food item: ${}'.format(mean_food_value))
 
+    print('\nQuestion 6')
+    print('Total sales of drink items: ${}'.format(total_drink_value))
+    print('Total sales of food items: ${}'.format(total_food_value))
+    print('Total sales of drinks are more than foods')
+
+    print('\nQuestion 7')
+    # Given a date, give the top 5 transactions for that day
+    print('The top 5 transactions for each weekday are listed below:')
+    for day in list_of_days:
+        print('{}'.format(day))
+        day_item_transaction_count = items_group_by_date[items_group_by_date['Weekday'] == day].groupby(['Weekday', 'Item']).agg('count').sort_values('Transaction', ascending=False)[0:5]
+        day_item_transaction_count.index = day_item_transaction_count.index.droplevel(0)
+        day_item_transaction_count.reset_index(inplace=True)
+        print(day_item_transaction_count.T.to_string(index=False))
+    print('There are some commonalities with popular items, but this list is not the same day to day')
+
+    print('\nQuestion 8')
+    print('The lowest 5 transactions for each weekday are listed below:')
+    print('If there are more than 5 items with 1 transaction, we arbitrarily list 5 items from that list ¯\_(\u30C4)_/¯')
+    for day in list_of_days:
+        print('{}'.format(day))
+        day_item_transaction_count = items_group_by_date[items_group_by_date['Weekday'] == day].groupby(['Weekday', 'Item']).agg('count').sort_values('Transaction', ascending=True)[0:5]
+        day_item_transaction_count.index = day_item_transaction_count.index.droplevel(0)
+        day_item_transaction_count.reset_index(inplace=True)
+        print(day_item_transaction_count.T.to_string(index=False))
+    print('There are very few items that share minimal popularity from day to day.')
+
+    print('\nQuestion 9')
+    print('I\'m going to assume this question is asking for how many drinks on average there are per transaction')
+    print('This lists the number of drinks per transaction:')
+    print(drinks_by_transactions.T)
+    print('This gets me my average number of drinks per transaction by getting the mean of all these transactions')
+    print('${}'.format(np.round(drinks_by_transactions[['Item']].mean().values[0], 2)))
 
 
 if __name__ == "__main__":
